584b7a06fe69fe17507d836032194027a09c619d
majianlord <chood@cna-consulting.com>
2022-02-14 08:15:00 -0600

    Update simple_youtube_stream.sh

--- youtube_stream_image/code/simple_youtube_stream.sh
+++ youtube_stream_image/code/simple_youtube_stream.sh
@@ -5,7 +5,7 @@
 
 VIDEO_DEVICE=${1:-video0}
 
-avconv -loglevel info -ar 44100 -ac 2 -f s16le -i /dev/zero \
-        -f video4linux2 -s 640x480 -r 10 -i /dev/${VIDEO_DEVICE} \
-        -vf "drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf:text='%{localtime\:%T}': fontcolor=white@0.8: x=0: y=0" \
+ffmpeg -loglevel info -ar 44100 -ac 2 -f s16le -i /dev/zero \
+        -f video4linux2 -s 640x640 -r 10 -i /dev/${VIDEO_DEVICE} \
+        -vf "drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf:text='%{localtime\:%T}': fontcolor=white@0.6: fontsize=12: x=0: y=0" \
         -qscale:v 2 -f flv rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_LIVE_KEY}
